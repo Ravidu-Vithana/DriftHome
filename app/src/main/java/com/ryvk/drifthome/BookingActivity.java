c0d59aceba28e01da0346658055456fa16e866d4
Ravidu Vithana <raviduyashith123@gmail.com>
2025-02-04 21:56:16 +0530

    completed the animation of booking ride btn. started the loader

--- app/src/main/java/com/ryvk/drifthome/BookingActivity.java
+++ app/src/main/java/com/ryvk/drifthome/BookingActivity.java
@@ -1,9 +1,15 @@
 package com.ryvk.drifthome;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +26,88 @@ public class BookingActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                initiateBooking();
+            }
+        }).start();
+
     }
+
+    private void initiateBooking(){
+        Button bookBtn = findViewById(R.id.button10);
+        Button cancelBtn = findViewById(R.id.button11);
+        TextView infoText = findViewById(R.id.textView14);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                infoText.setVisibility(View.VISIBLE);
+                bookBtn.setText("");
+                bookBtn.setTextSize(60f);
+                cancelBtn.setVisibility(View.GONE);
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int x = 3; x >= 0; x--){
+                    int finalX = x;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            bookBtn.setText(String.valueOf(finalX));
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        infoText.setVisibility(View.INVISIBLE);
+                        infoText.setText(R.string.d_booking_text1_driverOnTheWay);
+
+                        // Get new size and color values
+                        int newSize = (int) getResources().getDimension(R.dimen.d_circle_btn_small);
+                        int newColor = ContextCompat.getColor(getApplicationContext(), R.color.d_red1);
+
+                        // Update button height and width
+                        ViewGroup.LayoutParams params = bookBtn.getLayoutParams();
+                        params.width = newSize;
+                        params.height = newSize;
+                        bookBtn.setLayoutParams(params);
+
+                        // Change button background color
+                        bookBtn.setBackgroundColor(newColor);
+                        bookBtn.setText(R.string.d_booking_btn1_cancel);
+                        bookBtn.setTextSize(20f);
+
+                        float bookRideBtnAnimate = getResources().getDimension(R.dimen.d_bookRideBtn_animate);
+                        ObjectAnimator animator = ObjectAnimator.ofFloat(bookBtn, "y", bookRideBtnAnimate);
+                        animator.setDuration(500);
+                        animator.start();
+
+                        float bookRideBtnHeight = getResources().getDimension(R.dimen.d_circle_btn_small);
+                        ObjectAnimator animator2 = ObjectAnimator.ofFloat(infoText, "y", bookRideBtnAnimate + bookRideBtnHeight + 50f);
+                        animator2.setDuration(500);
+                        animator2.start();
+
+                    }
+                });
+            }
+        }).start();
+    }
+
 }
--- app/src/main/java/com/ryvk/drifthome/ui/home/HomeFragment.java
+++ app/src/main/java/com/ryvk/drifthome/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.ryvk.drifthome.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ryvk.drifthome.BaseActivity;
+import com.ryvk.drifthome.BookingActivity;
 import com.ryvk.drifthome.R;
 import com.ryvk.drifthome.databinding.FragmentHomeBinding;
 
@@ -23,9 +26,17 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), BookingActivity.class);
+                startActivity(i);
+            }
+        });
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
