6cfde491ce72ce702af598f96aa7309e4015d47d
Ravidu Vithana <raviduyashith123@gmail.com>
2025-02-06 23:30:44 +0530

    updated profile fragment

--- app/src/main/java/com/ryvk/drifthome/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ryvk/drifthome/ui/profile/ProfileFragment.java
@@ -24,8 +24,8 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textDashboard;
+//        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
